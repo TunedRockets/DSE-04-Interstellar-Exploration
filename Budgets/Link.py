8b4c404eb8a24d33089e70ee40bf7f6bfefebfb6
anwitagadgil <anwitagadgil1@gmail.com>
2026-04-30 10:49:19 +0200

    graph of link budget, still with madeup numbers

--- Budgets/Link.py
+++ Budgets/Link.py
@@ -29,35 +29,47 @@ def analog_to_bit(f_signal, nbps, N=2.2):
     print(f"{QE(nbps)} %")
     return f_signal*nbps*N
 
+def power_to_Db(P):
+    return 10*np.log(P)
 
 
 # universe parameters
 k = 1.38*10**(-23)          # Boltzmann constant (J/K)
+k_db = -23*power_to_Db(1.38)
+AU_to_m = 149597870691
+r_e = 6378000               # radius (m) of the earth
+d_e = 2*r_e                 # diameter (m) of the earth 
+step = 100
 
 # S/c antenna parameters
 spectrum_utilisation = 1        # no. of bits transmitted per unit frequency (bps/Hz)
-T = 500             # System noise temperature (K) - depends on frequency
-hpbw = (0.5, 0.5)       # half-power-beam-width (rad, rad) in spherical coordinate system (theta, phi)
+T = 13.5             # System noise temperature (K) - depends on frequency
+#hpbw = (0.5, 0.5)       # half-power-beam-width (rad, rad) in spherical coordinate system (theta, phi)
 eta_P = 0.5        # power  / power into the antenna
 eta_trans = 0.18         # Transmitter efficiency (from adsee yr 1)
 
 # G/s antenna parameters
-sn = 2.3            # Signal-to-Noise ratio (from requirements)
+sn = 1.3            # Signal-to-Noise ratio (from requirements)
 A_ant = 20.5            # g/s antenna frontal area (m^2)
 eta_ant = 0.6           # g/s antenna efficiency (from adsee yr 1)
 
 # Trajectory parameters
-t_gs = 1000     # time (s) between two consequetive passes over the ground station
-tr_time = 100       # time (s) to transmit per pass
+t_gs = 0.00001    # time (s) between two consequetive passes over the ground station
+tr_time = 1       # time (s) to transmit per pass
 
 # Power subsystem parameters
-P_in = 5            # Power (w) into the transmitter (from power subsystem)
+P_in_db = 250            # Power (w) into the transmitter (from power subsystem)
 
+# Mission parameters
+d_max = 3*AU_to_m            # max distance (m) from s/c to g/s
+hpbw = (d_e/d_max, d_e/d_max)       # small angle approx
+print(hpbw[0]*180/np.pi)
 
+d = np.linspace(20000, d_max, 100000) # distance (m) from s/c to g/s
 
 # Data in
 ## Payload
-data_radar = image_to_bit(3, (10, 10), 4, 10, 0.75) #example
+data_radar = image_to_bit(3, (10, 10), 3, 10, 0.75) #example
 data_payload = data_radar   # plus other things
 ## Housekeeping
 data_thermometer = analog_to_bit(10, 16)    #example
@@ -72,21 +84,47 @@ tr_max = data_in + memory_size/tr_time      # max telemetry bit-rate (bps)
 
 B = tr_max / spectrum_utilisation      # bandwidth (Hz)
 
-N0 = k*T            # Noise spectral density (W/Hz)
-N = N0*B               # Noise power (W)
+#N0 = k*T            # Noise spectral density (W/Hz)
+N0_db = k_db + power_to_Db(T)
+#N = N0*B               # Noise power (W)
+N_db = N0_db + power_to_Db(B)
 
 
-C_req = N*sn            # Required recieved power (W) at groundstation
+#C_req = N*sn            # Required recieved power (W) at groundstation
+C_req_db = N_db + power_to_Db(sn)
 
 D = 4*np.pi/(hpbw[0]*hpbw[1])       # Directivity
 G = eta_P*D         # Gain
+G_db = 400           # for Rosetta
 
-EIRP = P_in*eta_trans*G
-d = 10000000            # distance (m) from s/c to g/s
-Wf = EIRP/(4*np.pi*d*d)     # Power flux density (W/m^2)
 
-C_real = Wf*A_ant*eta_ant/2     # Actually recieved power (W) at the groundstation (worst case)
-print(C_real - C_req)
+#EIRP = P_in*eta_trans*G
+EIRP_db = P_in_db + power_to_Db(eta_trans) + G_db
+#Wf_min = EIRP/(4*np.pi*d_max*d_max)     # Power flux density (W/m^2)
+#Wf = EIRP/(4*np.pi*d*d)     # Power flux density (W/m^2)
+Wf_db = EIRP_db - power_to_Db(4*np.pi*d*d)
+
+#C_real_min = Wf[-1]*A_ant*eta_ant/2     # Actually recieved power (W) at the groundstation (worst case)
+C_real_min_db = Wf_db[-1] - power_to_Db(A_ant*eta_ant/2)
+
+#C_real = Wf*A_ant*eta_ant/2     # Actually recieved power (W) at the groundstation (worst case)
+C_real_db = Wf_db - power_to_Db(A_ant*eta_ant/2)
+print(C_real_db[-1] - C_req_db)
+
+print(P_in_db, N_db, EIRP_db, C_real_db[-1], C_req_db)
+# --- Plotting ---
+plt.figure(figsize=(10, 6))
+
+plt.plot(np.linspace(0, 10000, 10), np.repeat(P_in_db, 10))
+plt.plot(np.linspace(10000, 20000, 10),np.linspace(P_in_db, EIRP_db, 10))
+plt.plot(d, EIRP_db - power_to_Db(np.linspace(0.1, 4*np.pi*d_max*d_max, 100000)))
+plt.axline((20000, C_req_db), (100000, C_req_db))
+
+
+
+
+
+plt.show()
 
 
 

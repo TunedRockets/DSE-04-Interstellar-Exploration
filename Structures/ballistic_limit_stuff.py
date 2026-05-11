b65f7897ba9cafeb39f4be938b8c2662edf58b4b
Maria Nasaudean <mnasaudean@gmail.com>
2026-05-11 16:44:13 +0200

    more ball(i)s(tic) limit stuff

--- Structures/ballistic_limit_stuff.py
+++ Structures/ballistic_limit_stuff.py
@@ -23,7 +23,7 @@ gamma = 1/3
 delta = 4/3 # for theta >=65deg or theta <=45deg 
 epsilon = 8/3 # for theta >=65deg or theta <=45deg
 
-theta = np.radians(0) # angle of impact, 0 degrees for normal incidence...?
+# theta = np.radians(0) # angle of impact, 0 degrees for normal incidence...?
 
 # densities [g/cm^3]
 rho_p = 2.7
@@ -37,10 +37,10 @@ v = np.arange(0, 30.1, 0.1) # km/s
 # print(v)
 
 
-def calculate_d_c(v, S1, S2, vt1, vt2):
+def calculate_d_c(v, S1, S2, vt1, vt2, theta):
     return ((((t_w**alpha+t_b)/K_3S)*(sigma_y_ksi/40)**(1/2)+t_ob+K_MLI*t_eq_MLI)/(0.6*np.cos(theta)**delta*rho_p**(1/2)*v**(2/3)))**(18/19)
 
-def calculate_d_c_larger_than_vt2(v, S1, S2, vt1, vt2):
+def calculate_d_c_larger_than_vt2(v, S1, S2, vt1, vt2, theta):
     return (1.155*(S1**(1/3)*(t_b+K_tw*t_w)**(2/3)+(K_S2*S2**beta)*(t_w**gamma)*np.cos(theta)**(-epsilon))*(sigma_y_ksi/70)**(1/3))/((K_3D**(2/3))*(rho_p**(1/3))*(rho_ob**(1/9))*(v**(2/3))*(np.cos(theta)**delta))
 
 # velocities [km/s] vs type of material 
@@ -52,8 +52,8 @@ vt21 = 7
 vt12 = 4
 vt22 = 10
 
-def calculate_d_c_failure_rear_wall_in_shatter_veloc_regime(v, S1, S2, vt1, vt2):
-    return calculate_d_c(vt1, S1, S2, vt1, vt2) + (calculate_d_c_larger_than_vt2(vt2, S1, S2, vt1, vt2) - calculate_d_c(vt1, S1, S2, vt1, vt2)) / (vt2 - vt1) * (v - vt1)
+def calculate_d_c_failure_rear_wall_in_shatter_veloc_regime(v, S1, S2, vt1, vt2, theta):
+    return calculate_d_c(vt1, S1, S2, vt1, vt2, theta) + (calculate_d_c_larger_than_vt2(vt2, S1, S2, vt1, vt2, theta) - calculate_d_c(vt1, S1, S2, vt1, vt2, theta)) / (vt2 - vt1) * (v - vt1)
 
 # 1. Ensure your functions are defined to take (v, S1, constant)
 # def calculate_d_c(v, s, c): ...
@@ -68,24 +68,92 @@ funcs = [
 ]
 
 # 3. Calculate using the 'args' parameter to pass S1 and the constant
-y_S2_0  = np.piecewise(v, conditions1, funcs, S1, 0, vt11, vt21)
-y_S2_10 = np.piecewise(v, conditions1, funcs, S1, 10, vt11, vt21)
-y_S2_20 = np.piecewise(v, conditions1, funcs, S1, 20, vt11, vt21)
 
-y_S2_0_1  = np.piecewise(v, conditions2, funcs, S1, 0, vt12, vt22)
-y_S2_10_1 = np.piecewise(v, conditions2, funcs, S1, 10, vt12, vt22)
-y_S2_20_1 = np.piecewise(v, conditions2, funcs, S1, 20, vt12, vt22)
+# theta = 0
 
-plt.plot(v, y_S2_0)
-plt.plot(v, y_S2_10)
-plt.plot(v, y_S2_20)
-plt.plot(v, y_S2_0_1)
-plt.plot(v, y_S2_10_1)
-plt.plot(v, y_S2_20_1)
+# type 1
+y_S2_0_theta0  = np.piecewise(v, conditions1, funcs, S1, 0, vt11, vt21, theta=np.radians(0))
+y_S2_10_theta0 = np.piecewise(v, conditions1, funcs, S1, 10, vt11, vt21, theta=np.radians(0))
+y_S2_20_theta0 = np.piecewise(v, conditions1, funcs, S1, 20, vt11, vt21, theta=np.radians(0))
+y_S2_30_theta0 = np.piecewise(v, conditions1, funcs, S1, 30, vt11, vt21, theta=np.radians(0))
+y_S2_40_theta0 = np.piecewise(v, conditions1, funcs, S1, 40, vt11, vt21, theta=np.radians(0))
+
+# type 2
+y_S2_0_1_theta0  = np.piecewise(v, conditions2, funcs, S1, 0, vt12, vt22, theta=np.radians(0))
+y_S2_10_1_theta0 = np.piecewise(v, conditions2, funcs, S1, 10, vt12, vt22, theta=np.radians(0))
+y_S2_20_1_theta0 = np.piecewise(v, conditions2, funcs, S1, 20, vt12, vt22, theta=np.radians(0))
+y_S2_30_1_theta0 = np.piecewise(v, conditions2, funcs, S1, 30, vt12, vt22, theta=np.radians(0))
+
+
+# theta = 45
+
+# type 1
+y_S2_0_2_theta45  = np.piecewise(v, conditions1, funcs, S1, 0, vt11, vt21, theta=np.radians(45))
+y_S2_10_2_theta45 = np.piecewise(v, conditions1, funcs, S1, 10, vt11, vt21, theta=np.radians(45))
+y_S2_20_2_theta45 = np.piecewise(v, conditions1, funcs, S1, 20, vt11, vt21, theta=np.radians(45))
+y_S2_30_2_theta45 = np.piecewise(v, conditions1, funcs, S1, 30, vt11, vt21, theta=np.radians(45))
+
+# type 2
+y_S2_0_3_theta45  = np.piecewise(v, conditions2, funcs, S1, 0, vt12, vt22, theta=np.radians(45))
+y_S2_10_3_theta45 = np.piecewise(v, conditions2, funcs, S1, 10, vt12, vt22, theta=np.radians(45))
+y_S2_20_3_theta45 = np.piecewise(v, conditions2, funcs, S1, 20, vt12, vt22, theta=np.radians(45))
+y_S2_30_3_theta45 = np.piecewise(v, conditions2, funcs, S1, 30, vt12, vt22, theta=np.radians(45))
+
+def plot_ballistic_limit(v, y1, y2, y3, y4, y5 = None, labels=None):
+    # S2 = 0
+    plt.plot(v, y1)
+    plt.plot(v, y2)
+    plt.plot(v, y3)
+    plt.plot(v, y4)
+    plt.plot(v, y5) if y5 is not None else None
+    # plt.ylim(0, 1)
+    plt.xlabel('Velocity (km/s)')
+    plt.ylabel('Critical Diameter (cm)')
+    plt.title('Ballistic Limit vs Velocity for a certain S2')
+    plt.legend(labels if labels else ['type 1, theta = 0deg', 'type 2, theta = 0deg', 'type 1, theta = 45deg', 'type 2, theta = 45deg', 'type 1, theta = 0deg', 'type 2, theta = 0deg', 'type 1, theta = 45deg', 'type 2, theta = 45deg'])
+    plt.grid()
+    plt.show()
+
+plt.plot(v, y_S2_0_theta0)
+plt.plot(v, y_S2_10_theta0)
+plt.plot(v, y_S2_20_theta0)
+plt.plot(v, y_S2_0_1_theta0)
+plt.plot(v, y_S2_10_1_theta0)
+plt.plot(v, y_S2_20_1_theta0)
+plt.plot(v, y_S2_0_2_theta45)
+plt.plot(v, y_S2_10_2_theta45)
+plt.plot(v, y_S2_20_2_theta45)
+plt.plot(v, y_S2_0_3_theta45)
+plt.plot(v, y_S2_10_3_theta45)
+plt.plot(v, y_S2_20_3_theta45)
 # plt.ylim(0, 1)
 plt.xlabel('Velocity (km/s)')
 plt.ylabel('Critical Diameter (cm)')
 plt.title('Ballistic Limit vs Velocity')
-plt.legend(['S2 = 0 cm, type 1', 'S2 = 10 cm, type 1', 'S2 = 20 cm, type 1', 'S2 = 0 cm, type 2', 'S2 = 10 cm, type 2', 'S2 = 20 cm, type 2'])
+plt.legend(['S2 = 0 cm, type 1, theta = 0deg', 'S2 = 10 cm, type 1, theta = 0deg',
+             'S2 = 20 cm, type 1, theta = 0deg', 'S2 = 0 cm, type 2, theta = 0deg',
+               'S2 = 10 cm, type 2, theta = 0deg', 'S2 = 20 cm, type 2, theta = 0deg',
+                 'S2 = 0 cm, type 1, theta = 45deg', 'S2 = 10 cm, type 1, theta = 45deg',
+                   'S2 = 20 cm, type 1, theta = 45deg', 'S2 = 0 cm, type 2, theta = 45deg',
+                     'S2 = 10 cm, type 2, theta = 45deg', 'S2 = 20 cm, type 2, theta = 45deg'])
 plt.grid()
 plt.show()
+
+
+# # S2 = 0
+# plot_ballistic_limit(v, y_S2_0_theta0, y_S2_0_1_theta0, y_S2_0_2_theta45, y_S2_0_3_theta45, labels=['S2 = 0 cm, type 1, theta = 0deg', 'S2 = 0 cm, type 2, theta = 0deg', 'S2 = 0 cm, type 1, theta = 45deg', 'S2 = 0 cm, type 2, theta = 45deg'])
+
+# # S2 = 10
+# plot_ballistic_limit(v, y_S2_10_theta0, y_S2_10_1_theta0, y_S2_10_2_theta45, y_S2_10_3_theta45, labels=['S2 = 10 cm, type 1, theta = 0deg', 'S2 = 10 cm, type 2, theta = 0deg', 'S2 = 10 cm, type 1, theta = 45deg', 'S2 = 10 cm, type 2, theta = 45deg'])
+
+# # S2 = 20
+# plot_ballistic_limit(v, y_S2_20_theta0, y_S2_20_1_theta0, y_S2_20_2_theta45, y_S2_20_3_theta45, labels=['S2 = 20 cm, type 1, theta = 0deg', 'S2 = 20 cm, type 2, theta = 0deg', 'S2 = 20 cm, type 1, theta = 45deg', 'S2 = 20 cm, type 2, theta = 45deg'])
+
+# # S2 = 30
+# plot_ballistic_limit(v, y_S2_30_theta0, y_S2_30_1_theta0, y_S2_30_2_theta45, y_S2_30_3_theta45, labels=['S2 = 30 cm, type 1, theta = 0deg', 'S2 = 30 cm, type 2, theta = 0deg', 'S2 = 30 cm, type 1, theta = 45deg', 'S2 = 30 cm, type 2, theta = 45deg'])
+
+
+# S2 = {0, 10, 20, 30}, type 1, theta = 0deg
+plot_ballistic_limit(v, y_S2_0_theta0, y_S2_10_theta0, y_S2_20_theta0, y_S2_30_theta0, y_S2_40_theta0, labels=['S2 = 0 cm', 'S2 = 10 cm', 'S2 = 20 cm', 'S2 = 30 cm', 'S2 = 40 cm'])
+
+# NEED TO QUANTIFY!!! the mass increase for +10cm of S2
